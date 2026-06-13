fcf24199fb4917fb2453f73aa6cc85fd21354b6e
0neTV <onetv@local>
2026-06-14 00:37:39 +0800

    Simplify next.config.js for Cloudflare Pages compatibility

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-/* eslint-disable @typescript-eslint/no-var-requires */
 
 const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
-
   reactStrictMode: false,
   swcMinify: false,
-
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -21,60 +18,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-  },
-
-  webpack(config, { isServer }) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg')
-    );
-
-    if (fileLoaderRule) {
-      config.module.rules.push(
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/,
-        },
-        {
-          test: /\.svg$/i,
-          issuer: { not: /\.(css|scss|sass)$/ },
-          resourceQuery: { not: /url/ },
-          loader: '@svgr/webpack',
-          options: {
-            dimensions: false,
-            titleProp: true,
-          },
-        }
-      );
-
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
-    // Exclude Node.js-only packages from edge bundle
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-        crypto: false,
-        fs: false,
-        path: false,
-        stream: false,
-        http: false,
-        https: false,
-        zlib: false,
-      };
-    }
-
-    // Mark Node.js-only packages as external to prevent bundling errors
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@upstash/redis': false,
-      'redis': false,
-    };
-
-    return config;
   },
 };
 
